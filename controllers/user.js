ca779573101ed4e644bd8d74bbb0888bdb7446e1
Quan Vo <33823448+quanvophuong@users.noreply.github.com>
2022-09-05 16:08:38 +0700

    update

--- controllers/user.js
+++ controllers/user.js
@@ -8,30 +8,49 @@
 const User = require('../models/User');
 
 const index = async (req, res, next) => {
-    try {
 
-        const users = await User.find({});
-        return res.status(201).json(users);
-
-    } catch (error){
-        next(error);
-    }
+    const users = await User.find({});
+    return res.status(201).json(users);
 
 }
 
 const newUser = async (req, res, next) => {
-    try {
-        const newUser = new User(req.body);
-        await newUser.save();
 
-        return res.status(201).json({user: newUser});
-    } catch (err){
-        next(err);
-    }
+    const newUser = new User(req.body);
+    await newUser.save();
 
+    return res.status(201).json({user: newUser});
+}
+
+const getUser = async (req, res, next) => {
+
+    const { userID } = req.params;
+    const user = await User.findById(userID);
+
+    return res.status(200).json({user});
+}
+
+const replaceUser = async (req, res, next) => {
+    // enforce new user to old user
+    const { userID } = req.params;
+
+    const newUser = req.body;
+    console.log('user id: ',userID)
+    console.log('body: ',req.body);
+    // const result = await User.findByIdAndUpdate(userID, newUser);
+
+    return res.status(200).json({ message: 'true'});
+
+}
+
+const updateUser = async (req, res, next) => {
+    // number of fields
 }
 
 module.exports = {
     index,
-    newUser
+    newUser,
+    getUser,
+    replaceUser,
+    updateUser
 }
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const router = express.Router();
+// const router = express.Router();
+const router = require('express-promise-router')();
 
 const UserController = require('../controllers/user');
 
 router.route('/')
         .get(UserController.index)
         .post(UserController.newUser)
-        .patch()
-        .put()
-        .delete()
+
+router.route('/:userID')
+    .get(UserController.getUser)
+    .put(UserController.replaceUser)
+    .patch(UserController.updateUser)
 
 module.exports = router;
