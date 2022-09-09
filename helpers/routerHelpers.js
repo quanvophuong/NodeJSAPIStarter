6beb86aefa8c01fd0d07baf45b3e0615f31c6eee
Quan Vo <33823448+quanvophuong@users.noreply.github.com>
2022-09-09 17:06:27 +0700

    Update routerHelpers.js

--- helpers/routerHelpers.js
+++ helpers/routerHelpers.js
@@ -1,10 +1,23 @@
 const Joi = require('joi');
 
+const validateBody = (schema) => {
+    return (req, res, next) => {
+        const validatorResult = schema.validate(req.body);
+
+        if (validatorResult.error) {
+            return res.status(400).json(validatorResult.error);
+        } else {
+            if(!req.value) req.value = {};
+            if(!req.value['params']) req.value.params = {};
+
+            console.log(validatorResult)
+        }
+    }
+}
+
 const validateParam = (schema, name) => {
     return (req,res,next) => {
-        console.log('params ', req.params[name]);
         const validatorResult = schema.validate({param: req.params[name]});
-        console.log('result ',validatorResult);
 
         if(validatorResult.error) {
             return res.status(400).json(validatorResult.error);
@@ -13,7 +26,7 @@ const validateParam = (schema, name) => {
             if(!req.value['params']) req.value.params = {};
 
             req.value.params[name] = req.params[name];
-            return next();
+            next();
         }
 
 
